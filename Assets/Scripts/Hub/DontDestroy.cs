c72a9247fa4d8398c844e4ae4c4a4b4700f43811
LaPaellaMasquer <46828843+LaPaellaMasquer@users.noreply.github.com>
2023-02-25 17:45:04 +0100

    ✨save hub between switch scene

--- Assets/Scripts/Hub/DontDestroy.cs
+++ Assets/Scripts/Hub/DontDestroy.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
-    public string tag;
 
     private void Awake()
     {
-        GameObject[] levels = GameObject.FindGameObjectsWithTag(tag);
-
+        GameObject[] levels = GameObject.FindGameObjectsWithTag(gameObject.tag);
         if (levels.Length > 1)
         {
             DestroyImmediate(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+        SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
+    }
+
+    private void SceneManager_activeSceneChanged(Scene current, Scene next)
+    {
+        gameObject.SetActive(next.name == gameObject.tag);
     }
 }

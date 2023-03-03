2c50c155b8472f278ee2fce5d1a5fde33744a22b
Benjamin-Duez <benjamin.duez@hotmail.com>
2023-03-03 21:56:57 +0100

    :sparkles: detect touch input on piece

---
+++ Assets/Scripts/EnergyPuzzle/InputDetectionScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDetectionScript : MonoBehaviour
+{
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider != null)
+                {
+                    hit.collider.SendMessage("rotate");
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    hit.collider.SendMessage("rotate");
+                }
+            }
+        }
+
+#endif
+    }
+}
+

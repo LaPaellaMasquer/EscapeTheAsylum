0b4ac6425ba57ad622acac47ddbd00f3b2af19a6
Daveine Capucine <capucine_daveine@ens.univ-artois.fr>
2023-03-06 17:50:54 +0100

    📦 Box enigma

--- Assets/Scripts/BoxEnigma/BoxEnigma.cs
+++ Assets/Scripts/BoxEnigma/BoxEnigma.cs
@@ -8,15 +8,41 @@ public class BoxEnigma : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        StartCoroutine(VibrateDuration());
+            StartCoroutine(VibrateDuration());
 
     }
 
-    public IEnumerator VibrateDuration()
+    private IEnumerator VibrateDuration()
     {
-        Handheld.Vibrate();
-        yield return new WaitForSeconds(10.0f);
+        WaitForSeconds one = new WaitForSeconds(1.0f);
+        WaitForSeconds two = new WaitForSeconds(2.0f);
+        int durationShort = 200;
+        int durationLong = 700;
+        Vibration.Init();
+        Vibration.VibrateAndroid(durationLong);
+        yield return one;
+        Vibration.VibrateAndroid(durationShort);
+        yield return one;
+        Vibration.VibrateAndroid(durationShort);
+        yield return one;
+        Vibration.VibrateAndroid(durationShort);
+        yield return two;
+        Vibration.VibrateAndroid(durationLong);
+        yield return one;
+        Vibration.VibrateAndroid(durationLong);
+        yield return one;
+        Vibration.VibrateAndroid(durationLong);
+        yield return two;
+        Vibration.VibrateAndroid(durationLong);
+        yield return one;
+        Vibration.VibrateAndroid(durationShort);
+        yield return one;
+        Vibration.VibrateAndroid(durationShort);
+        yield return one;
+        Vibration.VibrateAndroid(durationLong);
+        yield return two;
+
+        //Handheld.Vibrate();
         //yield return new WaitForSeconds(1.0f);
         //Handheld.Vibrate();
         //yield return new WaitForSeconds(1.0f);

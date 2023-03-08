8ade5037998bcaec17797c6d6b3ac47322a73253
AlexandreFlanquart <104308588+AlexandreFlanquart@users.noreply.github.com>
2023-03-08 17:20:52 +0100

    :sparkles: Voice analyzer

--- Assets/Scripts/VoiceEnigm/VoiceListener.cs
+++ Assets/Scripts/VoiceEnigm/VoiceListener.cs
@@ -6,15 +6,12 @@ using UnityEngine.Android;
 
 public class VoiceListener : MonoBehaviour
 {
-    int qSamples = 1024;  // array size
+    int qSamples = 4096;  // array size
     float refValue  = 0.1f; // RMS value for 0 dB
-    float threshold = 0.02f;      // minimum amplitude to extract pitch
     float rmsValue ;   // sound level - RMS
     float dbValue ;    // sound level - dB
-    float pitchValue ; // sound pitch - Hz
     private float[] samples; // audio samples
-    private float[] spectrum; // audio spectrum
-    private float fSample;
+    int nbValue = 33;
 
     private string state;
     private int minFreq;
@@ -55,23 +52,40 @@ public class VoiceListener : MonoBehaviour
         }
 
         samples = new float[qSamples];
-        spectrum = new float[qSamples];
-        fSample = AudioSettings.outputSampleRate;
 
         StartListen();
         audioSource.Play();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < nbValue; i++)
         {
-            listDB.Add(0);
+            listDB.Add(25);
         }
-    }
+
+       // Color c1 = Color.white;
+       // Color c2 = new Color(1, 1, 1, 0);
+       // lineRenderer.SetColors(c1, c2);
+
+        StartCoroutine(ShowLine());
+
+
+        }
 
 
     void Update()
     {
 
 
+    }
+
+    IEnumerator ShowLine()
+    {
+       while(true)
+        {
+            AnalyzeSound();
+            RenderLine();
+
+            yield return new WaitForSeconds(.01f);
+        }
     }
 
     void AnalyzeSound()
@@ -87,48 +101,26 @@ public class VoiceListener : MonoBehaviour
         rmsValue = Mathf.Sqrt(sum / qSamples); // rms = square root of average
         dbValue = 20 * Mathf.Log10(rmsValue / refValue); // calculate dB
         dbValue += 160;
+        dbValue = (dbValue)*90/160; // normalize
          if (dbValue < 0) 
             dbValue = 0; 
-
-        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
-        float maxV = 0, currentVal = 0f;
-        int maxN = 0;// maxN is the index of max
-        for (i = 0; i < qSamples; i++)
-        {
-            currentVal = spectrum[i];
-            if (currentVal > maxV && currentVal > threshold)
-            {
-                maxV = currentVal;
-                maxN = i;
-            }
-        }
-        float freqN = maxN; // pass the index to a float variable
-        if (maxN > 0 && maxN < qSamples - 1)
-        { // interpolate index using neighbours
-            var dL = spectrum[maxN - 1] / spectrum[maxN];
-            var dR = spectrum[maxN + 1] / spectrum[maxN];
-            freqN += 0.5f * (dR * dR - dL * dL);
-        }
-        pitchValue = freqN * (fSample / 2) / qSamples; // convert index to frequency
-
+  
         //add current db
         listDB.Add(dbValue);
         listDB.RemoveAt(0);
-        //audioSource.Stop();
     }
+   
     public void RenderLine()
     {
-        int nb = listDB.Count();
-        lineRenderer.positionCount = nb;
+        lineRenderer.positionCount = nbValue;
 
-        Vector3[] listPts = new Vector3[nb];
-        int pos = 5;
-        for (int i = 0; i < nb; i++)
+        Vector3[] listPts = new Vector3[nbValue];
+        float pos = -14;
+        for (int i = 0; i < nbValue; i++)
         {
-            listPts[i] = new Vector3(pos, listDB[i]/3, 0);
-            pos += 5;
-        }
-       
+            listPts[i] = new Vector3(pos, (listDB[i])/4+18, 0);
+            pos += 0.5f;
+        } 
 
         lineRenderer.SetPositions(listPts);
     }
@@ -138,17 +130,13 @@ public class VoiceListener : MonoBehaviour
         GUI.skin.label.fontSize = Screen.width / 40;
 
         GUILayout.Label("\n State :" + state);
-
-         AnalyzeSound();
-         GUILayout.Label("\n RMS: " + rmsValue.ToString("F2") + " (" + dbValue.ToString("F1") + " dB)\n" + "Pitch: " + pitchValue.ToString("F0") + " Hz");
-
-        RenderLine();
+        GUILayout.Label("\n RMS: " + rmsValue.ToString("F2") + " (" + dbValue.ToString("F1") + " dB)\n");
     } 
 
     public void StartListen()
     {
         state = "StartListen";
-        audioSource.clip = Microphone.Start(null, true, 2, maxFreq);
+        audioSource.clip = Microphone.Start(null, true, 1, maxFreq);
     }
 
     public void EndListen()

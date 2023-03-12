1997da831bfab5d7eb5b45f7f86bd3df6c48ac78
LaPaellaMasquer <46828843+LaPaellaMasquer@users.noreply.github.com>
2023-03-12 14:39:18 +0100

    💫End Door

--- Assets/Scripts/Hub/End.cs
+++ Assets/Scripts/Hub/End.cs
@@ -2,15 +2,20 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class End : MonoBehaviour
 {
+    public VideoPlayer videoEnd;
+
     // Start is called before the first frame update
     void Start()
     {
         if(PlayerPrefs.HasKey("hiddentext") && PlayerPrefs.GetInt("hiddentext") != 0)
         {
-            transform.Rotate(new Vector3(0, 90, 0));
+            transform.DORotate(new Vector3(0, -145, 0), 2f);
+            GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().Stop();
+            videoEnd.Play();
         }
     }
 
